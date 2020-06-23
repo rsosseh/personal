2454f227df5640160f4be073f0c77c59f522fe40
Ray Sosseh <raysosseh@Rays-MacBook-Pro-2.local>
2020-06-23 11:35:06 -0400

    meta stuff

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,11 @@ const Home = () => (
       <title>hey there</title>
       <link rel="icon" href="/favicon.ico" />
       <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="robots" content="index, follow" />
+      <meta property="og:title" content="ray sosseh's page"></meta>
+      <meta name="description" content="my name's ray sosseh and i like making stuff for the internet" />
+      <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
     </Head>
     <Personal />
   </div>
